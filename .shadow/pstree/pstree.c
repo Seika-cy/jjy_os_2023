86e5a0f91ea4ad7d049d64f4e375e60b6ad16ed5
tracer-nju <tracer@nju.edu.cn>
2023-06-16 13:59:49 +0800

    Linux DESKTOP-MV0KHDO 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:59:49 up 22 min,  0 users,  load average: 0.08, 0.03, 0.01

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,12 +1,42 @@
-#include <stdio.h>
 #include <assert.h>
-#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+
+typedef struct TreeNode{
+  char name[256];
+  unsigned int pid;
+  unsigned int ppid;
+}TreeNode;
+
+struct children_table
+{
+
+};
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  // for (int i = 0; i < argc; i++) {
+  //   assert(argv[i]);
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // assert(!argv[argc]);
+  struct option opts[] = {{"show-pids", 0, NULL, 'p'},
+                          {"numeric-sort", 0, NULL, 'n'},
+                          {"version", 0, NULL, 'V'}};
+  char c;
+  int show_pids_flag = 0;
+  int numeric_sort_flag = 1;
+  while ((c = getopt_long(argc, argv, "pnV", opts, NULL)) != EOF) {
+    switch (c) {
+    case 'p':
+      show_pids_flag = 1;
+      break;
+    case 'n':
+      numeric_sort_flag = 1;
+      break;
+    case 'V':
+      fprintf(stderr, "pstree 0.0.1\n");
+      return 0;
+    }
   }
-  assert(!argv[argc]);
   return 0;
 }
